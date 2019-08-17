8358ffaaac830eaacace63550867c81597f1fab9
lolBlock <627796942@qq.com>
2019-08-17 08:51:55 +0800

    补充第9章的注释

--- chapter_09/battery.py
+++ chapter_09/battery.py
@@ -4,10 +4,17 @@
 class Battery:  # 从空白创建一个类时可以用()，也可以省略
     """创建一个电瓶类"""
 
-    def __init__(self, battery_size=70):
+    def __init__(self, battery_size=70):  # 构造函数，可以给属性指定默认值
         """初始化电瓶的属性"""
         self.battery_size = battery_size
 
     def describe_battery(self):
         """打印一条描述电瓶容量的消息"""
         print("本电瓶容量为" + str(self.battery_size) + "-kwh.")
+
+
+new_battery = Battery()  # 创建实例
+size = new_battery.battery_size  # 访问属性，也可修改
+new_battery.describe_battery()  # 调用方法
+
+
--- chapter_09/car_battery.py
+++ chapter_09/car_battery.py
@@ -2,11 +2,11 @@
 from battery import Battery
 
 
-class CarBattery(Battery):
+class CarBattery(Battery):  # 继承
     """创建一个汽车电瓶类"""
 
-    def __init__(self, car_brand='others'):
-        super().__init__()  # 先初始化父类的属性
+    def __init__(self, car_brand='others'):  # 子类的构造函数
+        super().__init__()  # 先初始化父类的属性，调用父类的构造函数
         self.car_brand = car_brand  # 再初始化汽车电瓶特有的属性
 
         if self.battery_size == 70:
